1e10ee6c9a48e67834fd353b648f162c283db380
ms <schaefer9111@gmail.com>
2023-07-02 18:11:44 +0200

    landing page v2

--- moveLogo.js
+++ moveLogo.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('header')
 const fgElement1 = document.querySelector('#fgLayer1')
 const fgElement2 = document.querySelector('#fgLayer2')
 const fgElement3 = document.querySelector('#fgLayer3')
@@ -9,13 +10,25 @@ window.onresize = function () {
 }
 
 onmousemove = function (e) {
-  let left = scale(e.clientX, w / 1.5, w, 0, 100)
-  fgElement1.style.right = left * 0.5 + 'px'
-  fgElement2.style.left = left * 0.5 + 'px'
-  fgElement3.style.left = left + 'px'
-  fgElement4.style.right = left + 'px'
+  let left = scale(e.clientX, w / 2, w, 0, 20)
+  fgElement1.style.left = 50 + (-1 * left) / 3 + '%'
+  fgElement2.style.left = 50 + left / 3 + '%'
+  fgElement3.style.left = 50 + left + '%'
+  fgElement4.style.left = 50 + -1 * left + '%'
 }
 
 function scale(num, in_min, in_max, out_min, out_max) {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
+
+header.addEventListener('scroll', function () {
+  let scrollTop = header.scrollTop
+  let scrollHeight = header.scrollHeight - header.clientHeight
+  let scrollFraction = scrollTop / scrollHeight
+
+  let left = scale(scrollFraction, 0, 1, -200, 200)
+  fgElement1.style.left = 50 + left / 3 + '%'
+  fgElement2.style.left = 50 - left / 3 + '%'
+  fgElement3.style.left = 50 + left + '%'
+  fgElement4.style.left = 50 - left + '%'
+})
